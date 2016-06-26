2762a73e881aa81fa6f4f088a36dce2727d5db25
Trace Smith <trace@inpersona3d.com>
2016-06-26 15:47:57 -0500

    initial commit

--- HW_Chapter2/blog/blog.py
+++
@@ -1,9 +0,0 @@
-import bottle
-
-@bottle.route('/')
-def home_page():
-	mythings = ["apple","golf","football"]
-	return bottle.template('template', username = 'Tsmith', things = mythings)
-
-bottle.debug(True)
-bottle.run(host = 'localhost', port = 8080)
