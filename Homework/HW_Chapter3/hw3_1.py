077827c9fc6d3a13fad8ee6b7c559a99cc97d34b
Trace Smith <trace@inpersona3d.com>
2016-06-26 21:37:37 -0500

    initial commit

--- Homework/HW_Chapter3/hw3_1.py
+++ Homework/HW_Chapter3/hw3_1.py
@@ -1,17 +1,17 @@
 #mongoimport -d school -c students < students.json
 
-import pymongo
-import sys
+from pymongo import MongoClient
+from sys import exc_info
 
 #Drops the lowest Homework score for each student in the collection:
 
 def lowest_hw(scores):
 	lowest = None
 	lowest_score = 101
-	for item in scores:
-		if((item)['type'] == "homework" and (item['score'] <lowest_score)):
+	for i in scores:
+		if((i)['type'] == "homework" and (i['score'] < lowest_score)):
 			#found a new bound
-			lowest = item
+			lowest = i
 			lowest_score = lowest['score']
 	return lowest
 
@@ -21,9 +21,9 @@ def remove_lowest(collection):
 		_id = student["_id"]
 		print "Looking at student {_id}:".format(_id=_id)
 		scores = student['scores']
-		lowest = find_lowest_hw(scores)
+		lowest = lowest_hw(scores)
 		if(lowest is not None):
-			print(" Removing hw grade of {score}").format(score-lowest['score'])
+			print(" Removing hw grade of {score}").format(score=lowest['score'])
 			scores.remove(lowest)
 			collection.update_one({'_id':_id},
 								{'$set':{'scores':scores}})
@@ -31,20 +31,20 @@ def remove_lowest(collection):
 			print "Could not find a homework score to process"
 
 def main():
-	'''Establishes a client and drops the lowest score for each student'''
+	'''Establishes a client and drops the lowest HW score for each student'''
 	host = 'localhost'
 	port = 27017
 	dbname = 'school'
 	coll_name = 'students'
 
-	client = MongoClient(host=host,port =port)
+	client = MongoClient(host=host,port=port)
 	db = client[dbname]
 	collection = db[coll_name]
 
 	print ("Removing lowest score from students in the {db}.{collection} "
            "namespace."
            ).format(db=db.name, collection=collection.name)
-    remove_lowest(collection=collection)
+	remove_lowest(collection=collection)
 
 if __name__ == "__main__":
 	main()
